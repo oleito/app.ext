2e13d6d446b7ae3e76c6c96681200f5e30361805
Leandro <oleito@gmail.com>
2019-12-23 00:13:12 -0300

    FormArray creado

--- src/app/componentes/seguimiento/seguimiento.component.ts
+++ src/app/componentes/seguimiento/seguimiento.component.ts
@@ -6,31 +6,13 @@ import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@ang
   templateUrl: './seguimiento.component.html',
   styleUrls: ['./seguimiento.component.css']
 })
-export class SeguimientoComponent implements OnInit {
-
-
-  seguimientoForm: FormGroup;
-  piezas: FormArray;
-
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.seguimientoForm = this.formBuilder.group({
-      marca: new FormControl('', Validators.required),
-      modelo: new FormControl('', Validators.required),
-      patente: new FormControl('', Validators.required),
-      seguro: new FormControl('', Validators.required),
-      orden: new FormControl('', Validators.required),
-
-      piezas: this.formBuilder.array([])
-    });
-  }
+export class SeguimientoComponent {
+  constructor(public fb: FormBuilder) { }
 
   /*################ Registration Form ################*/
-  registrationForm = this.formBuilder.group({
-    nombre:new FormControl(),
-    addDynamicElement: this.formBuilder.array([])
-  })
+  registrationForm = this.fb.group({
+    addDynamicElement: this.fb.array([])
+  });
 
   /*############### Add Dynamic Elements ###############*/
   get addDynamicElement() {
@@ -38,34 +20,12 @@ export class SeguimientoComponent implements OnInit {
   }
 
   addItems() {
-    this.addDynamicElement.push(this.formBuilder.control(''))
+    this.addDynamicElement.push(this.fb.control(''))
   }
 
   // Submit Registration Form
   onSubmit() {
     alert(JSON.stringify(this.registrationForm.value))
-  }
-
-
-  createItem(): FormGroup {
-    return this.formBuilder.group({
-      pieza: '',
-      accion: ''
-    });
-  }
-
-  addPieza(): void {
-    this.piezas = this.seguimientoForm.get('piezas') as FormArray;
-    this.piezas.push(this.createItem());
-  }
-
-  deletePieza(i: number) {
-    this.piezas.removeAt(i);
-  }
-
-  cargarSeguimiento() {
-    console.log(this.seguimientoForm.valid);
-    console.log(this.seguimientoForm.value);
   }
 
 }
